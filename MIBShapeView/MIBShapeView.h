8f61853d5680fdbf1e62821ad2e81f3db143b2d6
Ben Packard <ben.packard@gmail.com>
2013-10-24 16:52:11 -0400

    Initial Commit

---
+++ MIBShapeView/MIBShapeView.h
@@ -0,0 +1,13 @@
+//
+//  MIBShapeView.h
+//  MIBShapeView
+//
+//  Created by Ben Packard on 10/24/13.
+//  Copyright (c) 2013 Ben Packard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MIBShapeView : NSObject
+
+@end
---
+++ MIBShapeView/MIBShapeView.m
@@ -0,0 +1,13 @@
+//
+//  MIBShapeView.m
+//  MIBShapeView
+//
+//  Created by Ben Packard on 10/24/13.
+//  Copyright (c) 2013 Ben Packard. All rights reserved.
+//
+
+#import "MIBShapeView.h"
+
+@implementation MIBShapeView
+
+@end
